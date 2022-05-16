7f6d4b000854609f28aaf61b3a3ebb62a2bfc927
Samuel-Anumudu <samuelmaize55@yahoo.com>
2022-05-16 18:37:35 +0200

    added routing and navigation

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import './navigation.styles.scss'
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div className="logo">
+            B<span className="hook">hook</span>worm
+          </div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-links" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
